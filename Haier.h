b6079a2bf86c786e5f8823bfd9c72a150d5d6371
Matěj Nevlud <nevlud3@gmail.com>
2021-01-05 20:09:59 +0100

    Log fanmodes

--- Haier.h
+++ Haier.h
@@ -24,8 +24,8 @@ using namespace esphome::climate;
 
 #define FAN_SPEED   25
 #define FAN_MIN     02
-#define FAN_MIDDLE  01
-#define FAN_MAX     02
+#define FAN_MEDIUM  01
+#define FAN_HIGH     00
 #define FAN_AUTO    03
 
 #define SWING        27
@@ -64,12 +64,22 @@ protected:
     ClimateTraits traits() override {
         auto traits = climate::ClimateTraits();
         traits.set_supports_away(false);
+
         traits.set_supports_auto_mode(true);
         traits.set_supports_heat_mode(true);
         traits.set_supports_cool_mode(true);
+        traits.set_supports_dry_mode(true);
+        traits.set_supports_fan_only_mode(true);
+
+        traits.set_supports_fan_mode_auto(true);
+        traits.set_supports_fan_mode_low(true);
+        traits.set_supports_fan_mode_medium(true);
+        traits.set_supports_fan_mode_high(true);
+
         traits.set_visual_max_temperature(10);
         traits.set_visual_max_temperature(50);
         traits.set_visual_temperature_step(1.0f);
+        
         traits.set_supports_current_temperature(true);
 
         return traits;
@@ -106,8 +116,6 @@ public:
         ESP_LOGD("Haier", "POLL: %s ", raw.c_str());
     }
 
-public:
-
     void readData() {
         auto raw = getHex(data, sizeof(data));
         ESP_LOGD("Haier", "Readed message: %s ", raw.c_str());
@@ -138,10 +146,39 @@ public:
                 case MODE_HEAT:
                     mode = CLIMATE_MODE_HEAT;
                     break;
+                case MODE_ONLY_FAN:
+                    mode = CLIMATE_MODE_FAN_ONLY;
+                    break;
+                case MODE_DRY:
+                    mode = CLIMATE_MODE_DRY;
+                    break;
                 default:
                     mode = CLIMATE_MODE_AUTO;
             }
+
+            // FAN
+            switch(data[FAN_SPEED]) {
+                case FAN_MIN:
+                    fan_mode = CLIMATE_FAN_LOW;
+                    break;
+                case FAN_MEDIUM:
+                    fan_mode = CLIMATE_FAN_MEDIUM;
+                    break;
+                case FAN_HIGH:
+                    fan_mode = CLIMATE_FAN_HIGH;
+                    break;
+                case FAN_AUTO:
+                    fan_mode = CLIMATE_FAN_AUTO;
+                    break;
+                default:
+                    ESP_LOGD("Haier", "FAN: DEFAULT (%d)", (int) data[FAN_SPEED]);
+            }
+
+
         }
+
+
+        logStatus();
 
         this->publish_state();
     }
@@ -164,6 +201,37 @@ public:
                 case CLIMATE_MODE_COOL:
                     data[POWER] = POWER_ON;
                     data[MODE] = MODE_COOL;
+                    break;
+                case CLIMATE_MODE_FAN_ONLY:
+                    data[POWER] = POWER_ON;
+                    data[MODE] = MODE_ONLY_FAN;
+                    break;
+                case CLIMATE_MODE_DRY:
+                    data[POWER] = POWER_ON;
+                    data[MODE] = MODE_DRY;
+                    break;
+            }
+
+        if (call.get_fan_mode().has_value())
+            switch (call.get_fan_mode().value()) {
+                case CLIMATE_FAN_OFF:
+                    data[POWER] = POWER_OFF;
+                    break;
+                case CLIMATE_FAN_LOW:
+                    data[POWER] = POWER_ON;
+                    data[FAN_SPEED] = FAN_MIN;
+                    break;
+                case CLIMATE_FAN_MEDIUM:
+                    data[POWER] = POWER_ON;
+                    data[FAN_SPEED] = FAN_MEDIUM;
+                    break;
+                case CLIMATE_FAN_HIGH:
+                    data[POWER] = POWER_ON;
+                    data[FAN_SPEED] = FAN_HIGH;
+                    break;
+                case CLIMATE_FAN_AUTO:
+                    data[POWER] = POWER_ON;
+                    data[FAN_SPEED] = FAN_AUTO;
                     break;
             }
 
@@ -211,6 +279,92 @@ public:
             crc += message[i];
 
         return crc;
+    }
+
+    void logStatus() {
+
+        ESP_LOGD("Haier", "Current Status");
+
+        ESP_LOGD("Haier", "TEMPERATURE: (%d)", (int) data[TEMPERATURE]);
+        ESP_LOGD("Haier", "SET_TEMPERATURE: (%d)", (int) (data[SET_TEMPERATURE] + 16));
+
+        // POWER
+        switch(data[POWER]) {
+            case POWER_ON:
+                ESP_LOGD("Haier", "POWER: ON (%d)", (int) data[POWER]);
+                break;
+            case POWER_OFF:
+                ESP_LOGD("Haier", "POWER: OFF (%d)", (int) data[POWER]);
+                break;
+        }
+
+        // MODE
+        switch(data[MODE]) {
+            case MODE_COOL:
+                ESP_LOGD("Haier", "MODE: COOL (%d)", (int) data[MODE]);
+                break;
+            case MODE_HEAT:
+                ESP_LOGD("Haier", "MODE: HEAT (%d)", (int) data[MODE]);
+                break;
+            case MODE_SMART:
+                ESP_LOGD("Haier", "MODE: SMART (%d)", (int) data[MODE]);
+                break;
+            case MODE_ONLY_FAN:
+                ESP_LOGD("Haier", "MODE: FAN (%d)", (int) data[MODE]);
+                break;
+            case MODE_DRY:
+                ESP_LOGD("Haier", "MODE: DRY (%d)", (int) data[MODE]);
+                break;
+            default:
+                ESP_LOGD("Haier", "MODE: DEFAULT (%d)", (int) data[MODE]);
+        }
+
+        // FAN
+        switch(data[FAN_SPEED]) {
+            case FAN_MIN:
+                ESP_LOGD("Haier", "FAN: MIN (%d)", (int) data[FAN_SPEED]);
+                break;
+            case FAN_MEDIUM:
+                ESP_LOGD("Haier", "FAN: MEDIUM (%d)", (int) data[FAN_SPEED]);
+                break;
+            case FAN_HIGH:
+                ESP_LOGD("Haier", "FAN: MAX (%d)", (int) data[FAN_SPEED]);
+                break;
+            case FAN_AUTO:
+                ESP_LOGD("Haier", "FAN: AUTO (%d)", (int) data[FAN_SPEED]);
+                break;
+            default:
+                ESP_LOGD("Haier", "FAN: DEFAULT (%d)", (int) data[FAN_SPEED]);
+        }
+
+        // SWING
+        switch(data[SWING]) {
+            case SWING_OFF:
+                ESP_LOGD("Haier", "SWING: OFF (%d)", (int) data[SWING]);
+                break;
+            case SWING_VERTICAL:
+                ESP_LOGD("Haier", "SWING: VERTICAL (%d)", (int) data[SWING]);
+                break;
+            case SWING_HORIZONTAL:
+                ESP_LOGD("Haier", "SWING: HORIZONTAL (%d)", (int) data[SWING]);
+                break;
+            default:
+                ESP_LOGD("Haier", "SWING: DEFAULT (%d)", (int) data[SWING]);
+        }
+
+        // FRESH
+        switch(data[FRESH]) {
+            case FRESH_ON:
+                ESP_LOGD("Haier", "FRESH: ON (%d)", (int) data[FRESH]);
+                break;
+            case FRESH_OFF:
+                ESP_LOGD("Haier", "FRESH: OFF (%d)", (int) data[FRESH]);
+                break;
+            default:
+                ESP_LOGD("Haier", "FRESH: DEFAULT (%d)", (int) data[FRESH]);
+        }
+
+
     }
 };
 
